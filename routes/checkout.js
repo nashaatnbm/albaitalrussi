56ecb2cc7139b0fb9603c1f1aa50ed6873cf212d
nashaatnbm <nashaatmohamed000@gmail.com>
2026-02-26 11:30:15 +0400

    Update checkout.js

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,12 +1,12 @@
-const express = require(‘express’);
+const express = require('express');
 const router = express.Router();
-const checkoutController = require(’../controllers/checkoutController’);
-const { requireAuth } = require(’../middleware/auth’);
+const checkoutController = require('../controllers/checkoutController');
+const { requireAuth } = require('../middleware/auth');
 
-router.get(’/checkout’, requireAuth, checkoutController.getCheckout);
-router.post(’/checkout’, requireAuth, checkoutController.postCheckout);
-router.post(’/checkout/from-cart’, requireAuth, checkoutController.postCheckoutFromCart);
-router.get(’/orders/:id/success’, requireAuth, checkoutController.getOrderSuccess);
-router.get(’/my-orders’, requireAuth, checkoutController.getMyOrders);
+router.get('/checkout', requireAuth, checkoutController.getCheckout);
+router.post('/checkout', requireAuth, checkoutController.postCheckout);
+router.post('/checkout/from-cart', requireAuth, checkoutController.postCheckoutFromCart);
+router.get('/orders/:id/success', requireAuth, checkoutController.getOrderSuccess);
+router.get('/my-orders', requireAuth, checkoutController.getMyOrders);
 
 module.exports = router;
